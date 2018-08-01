fc45b3fe34c02bb56d56d2d6f3fbd92f63ad79b8
dpignato <david.pignato@gmail.com>
2018-08-01 17:56:52 +0900

    undefined answer

--- src/app/syn-quizz/syn-quizz.component.ts
+++ src/app/syn-quizz/syn-quizz.component.ts
@@ -16,12 +16,11 @@ export class SynQuizzComponent implements OnInit {
   private learningArray: String[] = [];
   private lg_src: string;
   private word: string;
+  private synonym: string;
   private answer1: string;
   private answer2: string;
   private answer3: string;
   private answer4: string;
-  private translation: string;
-  private wrong_translation: string;
   private score: number;
   private index: number;
   private quizzStart: boolean;
@@ -49,7 +48,51 @@ export class SynQuizzComponent implements OnInit {
 
   constructor() { }
 
+
+  nextQuizz(){  // refresh the quizz after each answer
+    if(!this.quizzStart) this.quizzStart = true;
+    if(this.index < this.learningArrayLength){
+      this.words.user_id = this.user_id;
+      this.words.current_word = this.word;
+      this.synonym = this.words.getSynonym(this.word);
+      console.log("word: " + this.word)
+      console.log("syn: " + this.synonym)
+      this.answer1 = this.words.getSynonym(this.word);
+      this.words.fetchVocalUrl(this.word);
+      this.word = this.words.getLearningArray()[this.index];
+      this.index++;
+    }
+
+      /*this.words.user_id = this.user_id;
+      this.words.current_word = this.word;
+
+      if(this.answer === this.translation) {  // the user responds correctly : he read the word and passes the testSuccess
+        this.score++;
+        this.words.findWordIdAndRead()
+        this.words.findWordIdAndSucceedTest()
+      }
+      else { this.words.findWordIdAndFailTest() } // the user responds wrong: he passes testFail
+
+      this.word = this.words.getLearningArray()[this.index];
+      this.translation = this.words.getLearningArray()[this.index + this.learningArrayLength]
+      this.words.fetchVocalUrl(this.word);       // to get the vocal synthesis of the current word
+
+      console.log(this.word  + ' : ' + this.translation)
+
+      this.index++;
+
+
+    }*/
+    else {
+      if(this.index <= this.learningArrayLength) this.index++;
+      this.word = 'End of Quizz'
+      this.translation = 'Congratulation !'
+    }
+
+  }
+
   ngOnInit() {
+    this.learningArray = this.words.getLearningArray()
   }
 
 }
